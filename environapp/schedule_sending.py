37a333f66be0f2bc17dfd130364a17211c2d2b63
bobalmighty8765 <74462320+bobalmighty8765@users.noreply.github.com>
2020-11-15 11:18:15 -0500

    Debugged

--- environapp/schedule_sending.py
+++ environapp/schedule_sending.py
@@ -4,11 +4,12 @@ import os
 from twilio.rest import Client
 phonenumber = int(input('what is your phone number?(insert +area code)'))
 shower = int(input('how long in minutes did you shower today?(please answer within 8:05. results will come then as well.)'))
-sink = int(input('how long in minutes did you use the tap? (please answer before 8:10)')
+sink = int(input('how long in minutes did you use the tap? (please answer before 8:10)'))
 account_sid = "ACde852f468e8a9bd6d64a68c3aa35ec16"
 yourpercentshow = 20/(shower*2.5)
 yourpercentsink = 11/(sink*2)
 auth_token = "3c9ecac073e105c34fa397ac3b3e254b"
+body7 = (shower*2.5+sink*2), "was the amount of water you used total",(yourpercentshow+yourpercentsink)/2,'was the percent of water you used today compared to the average person'
 client = Client(account_sid, auth_token)
 message1 = client.messages \
         .create(
@@ -32,23 +33,43 @@ message4 = client.messages \
                 body=sink,
                 from_='+13157562206',
                 to_=phonenumber,
-)
+                )
 message5 = client.messages \
-           body=sink*2,
-           from_='+131575622016',
-           to_=phonenumber,)
+           .create(
+               body=sink*2,
+               from_='+131575622016',
+               to_=phonenumber,)
 message6 = client.messages \
-           body=yourpercentsink,
-           from_='+131575622016',
-           to_=phonenumber,)
+           .create(
+               body=yourpercentsink,
+               from_='+131575622016',
+               to_=phonenumber,)
 message7 = client.messages \
-           body= (shower*2.5+sink*2), "gallons were used",((yourpercentshow+yourpercentsink)/2),"was your total percentage compared to the average person",
-           from_='+131575622016',
-           to_=phonenumber,)
-schedule.every().day.at("20:00").do(print(message1.sid)),
-schedule.every().day.at("20:05").do(print(message2.sid,"gallons were used"))
-schedule.every().day.at("20:06").do(print(message3.sid,"is your percentage compared to the average person")
-schedule.every().day.at("20:06").do(print(message4.sid)
-schedule.every().day.at("20:10").do(print(message5.sid,"was the gallons of water you used")
-schedule.every().day.at("20:11").do(print(message6.sid,"was your percentage of water used compared to the average person")
-schedule.every().day.at("20:12").do(print(message7.sid)
+           .create(
+body= body7,
+from_='+131575622016',
+to_=phonenumber
+)
+def m1():
+    print(message1.sid)
+def m2():
+    print(message2.sid,'gallons were used')
+def m3():
+    print(message3.sid,"is your percentage compared to the average person for shower water.")
+def m4():
+    print(message4.sid)
+def m5():
+    print(message5.sid,"was the gallons of water you used")
+def m6():
+    print(message6.sid,"was your percentage of water used compared to the average person")
+def m7():
+    print(message7.sid)
+
+schedule.every().day.at("20:00").do(m1),
+schedule.every().day.at("20:05").do(m2),
+schedule.every().day.at("20:06").do(m3),
+schedule.every().day.at("20:06").do(m4),
+schedule.every().day.at("20:10").do(m5),
+schedule.every().day.at("20:11").do(m6),
+schedule.every().day.at("20:12").do(m7),
+
